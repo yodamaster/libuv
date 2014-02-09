de3d9814b86c2c0edf019e24b60f8a8006995dce
Benjamin Saunders <ben.e.saunders@gmail.com>
2014-02-09 12:45:49 -0800

    fs: vectored IO API for filesystem read/write
    This improves API consistency with uv_read and uv_write and may
    improve efficiency for some uses. Vectored IO is emulated when the
    requisite system calls are unavailable.

--- test/test-spawn.c
+++ test/test-spawn.c
@@ -236,6 +236,7 @@ TEST_IMPL(spawn_stdout_to_file) {
   uv_file file;
   uv_fs_t fs_req;
   uv_stdio_container_t stdio[2];
+  uv_buf_t buf;
 
   /* Setup. */
   unlink("stdout_file");
@@ -264,8 +265,8 @@ TEST_IMPL(spawn_stdout_to_file) {
   ASSERT(exit_cb_called == 1);
   ASSERT(close_cb_called == 1);
 
-  r = uv_fs_read(uv_default_loop(), &fs_req, file, output, sizeof(output),
-      0, NULL);
+  buf = uv_buf_init(output, sizeof(output));
+  r = uv_fs_read(uv_default_loop(), &fs_req, file, &buf, 1, 0, NULL);
   ASSERT(r == 12);
   uv_fs_req_cleanup(&fs_req);
 
@@ -289,6 +290,7 @@ TEST_IMPL(spawn_stdout_and_stderr_to_file) {
   uv_file file;
   uv_fs_t fs_req;
   uv_stdio_container_t stdio[3];
+  uv_buf_t buf;
 
   /* Setup. */
   unlink("stdout_file");
@@ -319,8 +321,8 @@ TEST_IMPL(spawn_stdout_and_stderr_to_file) {
   ASSERT(exit_cb_called == 1);
   ASSERT(close_cb_called == 1);
 
-  r = uv_fs_read(uv_default_loop(), &fs_req, file, output, sizeof(output),
-      0, NULL);
+  buf = uv_buf_init(output, sizeof(output));
+  r = uv_fs_read(uv_default_loop(), &fs_req, file, &buf, 1, 0, NULL);
   ASSERT(r == 27);
   uv_fs_req_cleanup(&fs_req);
 
